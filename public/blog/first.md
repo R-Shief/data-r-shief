b5a28c6eefc644630eb2a981a7d3bd2d37dfc3a3
Josh Bevan <josh.bevan@lfest.org>
2020-07-14 01:40:13 -0700

    Added a markdown blog

---
+++ public/blog/first.md
@@ -0,0 +1,11 @@
+# h1 Heading
+## h2 Heading
+### h3 Heading
+#### h4 Heading
+##### h5 Heading
+###### h6 Heading
+paragraph
+
+![Minion](https://octodex.github.com/images/minion.png)
+
+![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")
---
+++ routes/blog.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var md = require("markdown-it")({html: true, linkify: true, typographer: true});
+
+var hljs = require('highlight.js'); // https://highlightjs.org/
+
+// Actual default values
+var md = require('markdown-it')({
+  html: true, linkify: true, typographer: true,
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (__) {}
+    }
+
+    return ''; // use external default escaping
+  }
+});
+
+//router.use(express.json());
+
+router.get('/', function(req, res, next) {
+  const posts = fs.readdirSync(__dirname + '/../public/blog').filter(file => file.endsWith('.md'));
+  res.render('blog', {posts: posts});
+});
+
+router.get('/:article', function(req, res, next) {
+  const file = fs.readFile(__dirname + '/../public/blog/' + req.params.article + '.md', 'utf8', function(err, data) {
+    if (err) throw err;
+    var result = md.render(data);
+    console.log(result);
+    res.render('post', {post: result});
+  });
+});
+
+module.exports = router;
