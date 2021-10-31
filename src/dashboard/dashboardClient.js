086c199a616290ded08056db46b3eafc7a60b5b1
Josh Bevan <josh.bevan@lfest.org>
2021-10-30 22:28:35 -0700

    Added a query parameter check to disable the Wix headers manually

--- src/dashboard/dashboardClient.js
+++ src/dashboard/dashboardClient.js
@@ -1,6 +1,13 @@
 let Dash = require('./components/Dash.js');
 
 $( document ).ready(() => {
+
+  if ((new URLSearchParams(window.location.search)).get("nowix")) {
+    includeWix = false;
+  }
+
   window.history.replaceState({}, null, "/dashboard");
+
+
   ReactDOM.render(<Dash filterDefaults={filterDefaults} includeWix={includeWix} overviewUpOnStartup={overviewUpOnStartup} />, document.getElementById("root"));
 });
